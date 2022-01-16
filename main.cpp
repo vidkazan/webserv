032742a586d7a49e2b2533f92fcce8e73508b660
fcody <vid.kazan@gmail.com>
2022-01-16 22:56:50 +0300

    readRequest generateResponce

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main()
 	for(int i = 2000; i < 2003; i++)
 		webserv.addListenSocket(i,"127.0.0.1");
 	std::cout << "server start\n";
-	for(std::vector<Socket>::iterator it = webserv.getServSockets().begin();it != webserv.getServSockets().end(); it++)
+	for(std::vector<ListenSocket>::iterator it = webserv.getServSockets().begin();it != webserv.getServSockets().end(); it++)
 		listen(it->getSocketFD(), 1000);
 	for(;;)
 	{
@@ -14,29 +14,30 @@ int main()
 		fd_set readfds, writefds;
 		FD_ZERO(&readfds);
 		FD_ZERO(&writefds);
-		for(std::vector<Socket>::iterator it = webserv.getServSockets().begin();it != webserv.getServSockets().end(); it++)
+		for(std::vector<ListenSocket>::iterator it = webserv.getServSockets().begin();it != webserv.getServSockets().end(); it++)
 		{
 			FD_SET(it->getSocketFD(), &readfds);
 			if(it->getSocketFD() > largestFD)
 				largestFD = it->getSocketFD();
 		}
-		for(std::vector<int>::iterator it = webserv.getClientSockets().begin();it != webserv.getClientSockets().end(); it++)
+		for(std::vector<Client>::iterator it = webserv.getClientSockets().begin();it != webserv.getClientSockets().end(); it++)
 		{
-			FD_SET(*it, &readfds);
-			FD_SET(*it, &writefds);
-			if(*it > largestFD)
-				largestFD = *it;
+			FD_SET(it->getSocketFD(), &readfds);
+			if(it->getWriteStatus() == 1)
+				FD_SET(it->getSocketFD(), &writefds);
+			if(it->getSocketFD() > largestFD)
+				largestFD = it->getSocketFD();
 		}
 		std::cout << "select: waiting for connection " <<"\n";
-		if(select(largestFD + 1, &readfds, 0,0,0) < 0)
+		if(select(largestFD + 1, &readfds, &writefds,0,0) < 0)
 		{
 			webserv.errorMsg("webserv: select error");
 			exit(EXIT_FAILURE);
 		}
 		int fd;
-		for(std::vector<Socket>::iterator it = webserv.getServSockets().begin();it != webserv.getServSockets().end(); it++)
+		for(std::vector<ListenSocket>::iterator it = webserv.getServSockets().begin();it != webserv.getServSockets().end(); it++)
 		{
-			std::cout << "isset check ld: " << it->getSocketFD() << "\n";
+//			std::cout << "isset check ld: " << it->getSocketFD() << "\n";
 			if(FD_ISSET(it->getSocketFD(), &readfds))
 			{
 				struct sockaddr_in adrAccept = {0};
@@ -48,43 +49,33 @@ int main()
 					exit(EXIT_FAILURE);
 				}
 				fcntl(fd, F_SETFL, O_NONBLOCK);
-				std::cout << "add client socket fd: " << fd << "\n";
 				webserv.addClientSocket(fd);
 			}
 		}
-		for(std::vector<int>::iterator it = webserv.getClientSockets().begin();it != webserv.getClientSockets().end(); it++)
+		for(std::vector<Client>::iterator it = webserv.getClientSockets().begin();it != webserv.getClientSockets().end(); it++)
 		{
 			if(webserv.getClientSockets().empty())
 				break;
-			std::cout << "isset check fd: " << *it << "\n";
-			if (FD_ISSET(*it, &readfds)){
-				std::cout << "select: read ready on fd " << *it << "\n";
-				char buf[100000];
-				read(*it, &buf, 100000);
-				write(2, YELLOW, 5);
-				write(2, &buf, ft_strlen(buf));
-				write(2, WHITE, 5);
-				write(2, "\n", 1);
+//			std::cout << "isset check fd: " << it->getSocketFD() << "\n";
+			if (FD_ISSET(it->getSocketFD(), &readfds)){
+				std::cout << "select:"<< YELLOW << " read "<< WHITE << "ready on fd " << it->getSocketFD() << "\n";
+				webserv.readRequest(it);
+				if(it->getReadStatus() == 1)
+				{
+					webserv.generateResponse(it);
+				}
 			}
-			if(FD_ISSET(*it, &writefds))
+			if(FD_ISSET(it->getSocketFD(), &writefds))
 			{
-				std::cout << "select: write ready on fd " << *it << "\n";
-				char bufResp[] = "HTTP/1.1 200 OK\n"
-								 "Content-Length: 64\n"
-								 "Content-Type: text/html\n"
-								 "Connection: Closed\n\n"
-								 "<html>\n"
-								 "<body>\n"
-								 "<h1>Hello, World!!!!!!!!!!!!!</h1>\n"
-								 "</body>\n"
-								 "</html>";
+				std::cout << "select:"<< GREEN << " write "<< WHITE << "ready on fd " << it->getSocketFD() << "\n";
+//				char * newstr = it->getResponce()->c_str();
 				write(2, GREEN,5);
-				write(2, &bufResp, ft_strlen(bufResp));
+				write(2, it->getResponce()->c_str(), it->getResponce()->size());
 				write(2, WHITE,5);
 				write(2, "\n", 1);
-				write(*it, &bufResp, ft_strlen(bufResp));
-				close(*it);
-				std::cout << "close: " << *it << "\n";
+				write(it->getSocketFD(), it->getResponce()->c_str(), it->getResponce()->size());
+				close(it->getSocketFD());
+				std::cout << "close fd: " << it->getSocketFD() << "\n";
 				webserv.getClientSockets().erase(it);
 			}
 		}
--- webserv.hpp
+++ webserv.hpp
@@ -14,14 +14,47 @@
 #define YELLOW "\e[93m"
 #define GREEN "\e[92m"
 #define WHITE "\e[39m"
+#define WRITEREADY 1
 
+class Client {
+private:
+	int _socketFD;
+	int _isReadyForWrite;
+	int _readIsDone;
+	std::string* _request;
+	std::string* _responce;
+public:
+	Client(int fd): _socketFD(fd), _isReadyForWrite(0), _readIsDone(0){
+		_request = new std::string;
+		_responce = new std::string;
+	};
+	~Client(){};
+	std::string* getRequest(){return _request;};
+	std::string* getResponce(){return _responce;};
+	int getSocketFD(){return _socketFD;};
+	int getWriteStatus(){return _isReadyForWrite;}
+	int getReadStatus(){return _readIsDone;}
 
-class Socket {
+	void setWriteIsReady(){
+		_isReadyForWrite = 1;
+	};
+	void setRequest(std::string* req){
+		delete _request;
+		_request = req;
+	};
+	void setResponce(std::string* resp){
+		delete _responce;
+		_responce = resp;
+	};
+	void setReadStatusIsDone(){_readIsDone = 1;};
+};
+
+class ListenSocket {
 private:
 	int _socketFD;
 	struct sockaddr_in _adr;
 public:
-	Socket(int portNum, const char *stringIP){
+	ListenSocket(int portNum, const char *stringIP){
 		_adr.sin_addr.s_addr = 0;
 		_adr.sin_family = 0;
 		_adr.sin_len = 0;
@@ -34,35 +67,31 @@ public:
 		_adr.sin_port = htons(portNum);
 		_adr.sin_addr.s_addr = inet_addr(stringIP);
 	};
-	~Socket(){};
+	~ListenSocket(){};
 	int getSocketFD(){return _socketFD;};
 	struct sockaddr_in& getSockAddrInStruct(){return _adr;};
 };
 
 class Webserv {
 private:
-	std::vector<Socket> _servSockets;
-	std::vector<int> _clientSockets;
-	std::vector<int> _writeReadySockets;
+	std::vector<ListenSocket> _servSockets;
+	std::vector<Client> _clientSockets;
+
 public:
 	Webserv(){};
 	~Webserv(){};
 	void configFileParce(){};
-	std::vector<Socket>& getServSockets(){return _servSockets;};
-	std::vector<int>& getClientSockets(){return _clientSockets;};
-	std::vector<int>& getWriteReadySockets(){return _writeReadySockets;};
+	std::vector<ListenSocket>& getServSockets(){return _servSockets;};
+	std::vector<Client>& getClientSockets(){return _clientSockets;};
 	int getCurrentServSocketFDByIndex(int index){
 		return _servSockets[index].getSocketFD();
 	}
 	int getCurrentClientSocketFDByIndex(int index){
-		return _clientSockets[index];
-	}
-	int getCurrentWriteReadySocketFDByIndex(int index){
-		return _writeReadySockets[index];
+		return _clientSockets[index].getSocketFD();
 	}
 	void errorMsg(const char *msg){write(2, msg, strlen(msg));};
 	void addListenSocket(int portNum, char *stringIP){
-		Socket socket(portNum, stringIP);
+		ListenSocket socket(portNum, stringIP);
 		_servSockets.push_back(socket);
 		if(bind(_servSockets.back().getSocketFD(), (struct sockaddr *)&_servSockets.back().getSockAddrInStruct(), sizeof(_servSockets.back().getSockAddrInStruct())) < 0){
 			perror("bind error");
@@ -70,10 +99,35 @@ public:
 		}
 	}
 	void addClientSocket(int fd){
-		_clientSockets.push_back(fd);
+		Client clientSocket(fd);
+		_clientSockets.push_back(clientSocket);
 	}
-	void addWriteReadySocket(int fd){
-		_writeReadySockets.push_back(fd);
+	void readRequest(std::vector<Client>::iterator it){
+		char buf[1000];
+		bzero(&buf, 1000);
+		int ret = read(it->getSocketFD(), &buf, 1000);
+		write(2, YELLOW, 5);
+		write(2, &buf, ret);
+		write(2, WHITE, 5);
+		write(2, "\n", 1);
+		std::string *buf2 = new std::string(buf);
+		it->setRequest(buf2);
+		it->setReadStatusIsDone();
+	}
+	void generateResponse(std::vector<Client>::iterator it){
+		char bufResp[] = "HTTP/1.1 200 OK\n"
+						 "Content-Length: 64\n"
+						 "Content-Type: text/html\n"
+						 "Connection: Closed\n\n"
+						 "<html>\n"
+						 "<body>\n"
+						 "<h1>Hello, World!!!!!!!!!!!!!</h1>\n"
+						 "</body>\n"
+						 "</html>";
+		std::string *resp = new std::string(bufResp);
+		it->setResponce(resp);
+		std::cout << "write ready on fd: " << it->getSocketFD() << "\n";
+		it->setWriteIsReady();
 	}
 };
 
