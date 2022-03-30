e1239a6dd46cafd399c616854a7b1cec62f289b8
Carlos Venkman <cvenkman@si-f3.kzn.21-school.ru>
2022-03-30 04:33:10 +0300

    fix autoindex and cgi

--- src/other/utils.cpp
+++ src/other/utils.cpp
@@ -1,11 +1,8 @@
 #include "../main.hpp"
 
 void printLog(std::string description,std::string msg,std::string color){
-	(void)description;
-	(void)msg;
-	(void)color;
-	// std::cout << color;
-	// if(!description.empty())
-	// 	std::cout << description << "\n";
-	// std::cout << "|" << msg << "|" << WHITE << "\n";
+	std::cout << color;
+	if(!description.empty())
+		std::cout << description << "\n";
+	std::cout << "|" << msg << "|" << WHITE << "\n";
 }
